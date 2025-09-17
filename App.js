af91078f5d314a2b030e88621e5baf21a4ed4ff7
FelipeDaSilvaMachado <felipe.silvamachado@yahoo.com>
2025-09-17 19:19:52 -0300

    Aplicando try catch funcao async no useEffect para corrigir o consumo dos dados

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { FlatList, Text, View, ActivityIndicator } from 'react-native';
 import styles from './components/style.js';
 
-const request = async (callback) => {
-  const response = await fetch('https://api.disneyapi.dev/character');
-  const parsed = await response.json();
-  return parsed.data;
+const request = async () => {
+  try {
+    const response = await fetch('https://api.disneyapi.dev/character');
+    const parsed = await response.json();
+    return parsed.data;
+  } catch (error) {
+    console.error("Erro ao buscar os dados: ", error);
+    return [];
+  }
 }
 
 export default function App() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    request(setData);
-  }, [])
+    const buscandoDados = async () => {
+      const dadosConsumidos = await request();
+      setData(dadosConsumidos);
+      setIsLoading(false);
+    };
+    buscandoDados();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'#0000ff'} />
+      </View>
+    )
+  }
 
   return (
     <View style={styles.container}>
@@ -24,12 +43,13 @@ export default function App() {
       </View>
       <FlatList
         data={data}
-        keyExtractor={(item) => item._id}
-        renderItem={({item}) =>
+        keyExtractor={(item) => item._id.toString()}
+        renderItem={({ item }) =>
           <Text style={styles.item}>
-            <Text>Filmes: {item.films[0]}{'\n'}</Text>
-            <Text>Programa de TV: {item.tvShows[0]}{'\n'}</Text>
-            <Text>Video games: {item.videoGames[0]}{'\n'}</Text>
+            Id: {item._id}{'\n'}
+            Filmes: {item.films[0]}{'\n'}
+            Programa de TV: {item.tvShows[0]}{'\n'}
+            Video games: {item.videoGames[0]}{'\n'}
           </Text>
         }>
       </FlatList >
